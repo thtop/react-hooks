10cff8afd82ef79cfab10019d4b2fc8c791d908a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-05 15:34:13 +0700

    Finished Video Player Project

--- src/components/containers/Playlist.js
+++ src/components/containers/Playlist.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import PlaylistHeader from '../PlaylistHeader';
-import PlaylistItems from '../containers/PlaylistItems';
-import Nightmode from '../Nightmode';
+import React from "react";
+import PlaylistHeader from "../PlaylistHeader";
+import PlaylistItems from "../containers/PlaylistItems";
+import NightMode from "../Nightmode";
+import StyledPlaylist from "../styles/StyledPlaylist";
 
-const Playlist = props => {
-  return (
-    <div>
-      <Nightmode />
-      <PlaylistHeader />
-      <PlaylistItems />
-    </div>
-  );
-};
+const Playlist = ({ videos, active, nightModeCallback, nightMode }) => (
+  <StyledPlaylist>
+    <NightMode nightModeCallback={nightModeCallback} nightMode={nightMode} />
+    <PlaylistHeader active={active} total={videos.length} />
+    <PlaylistItems videos={videos} active={active} />
+  </StyledPlaylist>
+);
 
 export default Playlist;
--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import PlaylistItem from '../PlaylistItem';
+import withLink from '../hoc/withLink';
+import StyledPlaylistitems from '../styles/StyledPlaylistitems';
 
-const Playlistitems = props => <PlaylistItem />;
+const PlaylistItemWithLink = withLink(PlaylistItem);
+
+const Playlistitems = ({ videos, active }) => (
+  <StyledPlaylistitems>
+    {videos.map(video => (
+      <PlaylistItemWithLink
+        key={video.id}
+        video={video}
+        active={video.id === active.id ? true : false}
+        played={video.played}
+      />
+    ))}
+  </StyledPlaylistitems>
+)
 
 export default Playlistitems;
