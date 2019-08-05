dbbae8aef9d562ac5e6b7cdb9bb1ae105d325f1e
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-05 14:25:58 +0700

    Finidhed item 3 Setting up React Router

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,14 @@
-import React from "react";
-import WbnPlayer from "./WbnPlayer";
+import React from 'react';
+import WbnPlayer from './WbnPlayer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const App = () => <WbnPlayer />;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={WbnPlayer} />
+      <Route exact path="/:activeVideo" component={WbnPlayer} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
